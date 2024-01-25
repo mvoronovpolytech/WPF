8c50326a316524464acd3fa8223bb37aee61de2d
Maksym Voronov <mvoronov.polytech@gmail.com>
2024-01-25 11:15:52 +0200

    Implement Todo app

---
+++ TodoApp/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace TodoApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void addTaskButton_Click(object sender, RoutedEventArgs e)
+        {
+            var todoItem = new CheckBox();
+            todoItem.Content = taskInput.Text;
+            todoItem.Width = 200;
+            todoItem.Height = 24;
+            tasksPanel.Children.Add(todoItem);
+            taskInput.Clear();
+
+        }
+
+        private void taskInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            this.addTaskButton.IsEnabled = !string.IsNullOrWhiteSpace(taskInput.Text);
+        }
+    }
+}
+
