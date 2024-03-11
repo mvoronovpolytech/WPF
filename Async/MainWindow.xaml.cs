37eae1e7df74dcd7d3dd10ffb3e78bcbd763d572
Maksym Voronov <mvoronov.polytech@gmail.com>
2024-03-11 22:37:18 +0200

    Initi async project demo example

---
+++ Async/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Async
+{
+
+    public partial class MainWindow : Window
+    {
+        int _counter = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void IncrementButton_Click(object sender, RoutedEventArgs e)
+        {
+            _counter++;
+            LabelCounter.Content = _counter.ToString();
+        }
+
+        private void LoadData_Click(object sender, RoutedEventArgs e)
+        {
+            Thread.Sleep(5000);
+        }
+    }
+}
